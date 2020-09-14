10baef890c0af33e7630e2e8acc37b10771f9c4e
N.U <n.n.ushakova@gmail.com>
2020-09-14 10:08:11 -0400

    Redeclare variables acc.to es6

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,67 @@
 
 console.log("app.js is running!!");
 
-var template = React.createElement(
-  "p",
-  null,
-  " JSX from app.js"
+var webpage = {
+    title: "Random Choice",
+    subtitle: "JSX from app.js file",
+    options: ["One", "Two"]
+
+    //render subtitle only if it actually exists
+};var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        webpage.title
+    ),
+    webpage.subtitle && React.createElement(
+        "p",
+        null,
+        " ",
+        webpage.subtitle
+    ),
+    React.createElement(
+        "p",
+        null,
+        webpage.options.length > 0 ? "Here are the options:" : "There are no options"
+    )
 );
+
+var user = {
+    name: "Elisa",
+    location: "SP",
+    age: 26
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            "p",
+            null,
+            "Location: ",
+            location
+        );
+    }
+}
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        user.name ? user.name : "Anonimous"
+    ),
+    user.age && user.age >= 18 && React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    getLocation(user.location)
+);
+
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,42 @@
 console.log("app.js is running!!");
 
-var template = <p> JSX from app.js</p>;
-var appRoot = document.getElementById("app");
+const webpage = {
+    title: "Random Choice",
+    subtitle: "JSX from app.js file",
+    options: ["One", "Two"]
+}
+
+
+//render subtitle only if it actually exists
+const template = (
+    <div>
+        <h1>{webpage.title}</h1>
+        {webpage.subtitle && <p> {webpage.subtitle}</p>}
+    <p>{webpage.options.length > 0? "Here are the options:" : "There are no options"}</p>
+    </div>
+);
+
+const user = {
+    name: "Elisa",
+    location: "SP",
+    age: 26
+};
+
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+}
+
+const templateTwo = (
+    <div>
+        <h1>{user.name? user.name: "Anonimous"}</h1>
+        {(user.age && user.age>=18) && <p>Age: {user.age}</p>}        
+        {getLocation(user.location)}
+        
+    </div>    
+);
+
+const appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
