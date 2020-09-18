b3263bf3e9ca6a664436b57435f2274d762ac746
N.U <n.n.ushakova@gmail.com>
2020-09-18 12:40:59 -0400

    Add render() function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,64 +5,74 @@ console.log("app.js is running!!");
 var webpage = {
     title: "Random Choice",
     subtitle: "JSX from app.js file",
-    options: ["One", "Two"]
-
-    //render subtitle only if it actually exists
-};var template = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        webpage.title
-    ),
-    webpage.subtitle && React.createElement(
-        "p",
-        null,
-        " ",
-        webpage.subtitle
-    ),
-    React.createElement(
-        "p",
-        null,
-        webpage.options.length > 0 ? "Here are the options:" : "There are no options"
-    )
-);
-
-var user = {
-    name: "Elisa",
-    location: "SP",
-    age: 26
+    options: []
 };
 
-function getLocation(location) {
-    if (location) {
-        return React.createElement(
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault(); //prevent fullpage refresh
+    var option = e.target.elements.option.value; //get value from form
+
+    if (option) {
+        webpage.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
+};
+
+var render = function render() {
+    //render subtitle only if it actually exists
+    var template = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            webpage.title
+        ),
+        webpage.subtitle && React.createElement(
             "p",
             null,
-            "Location: ",
-            location
-        );
-    }
-}
-
-var templateTwo = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        user.name ? user.name : "Anonimous"
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        "p",
-        null,
-        "Age: ",
-        user.age
-    ),
-    getLocation(user.location)
-);
+            " ",
+            webpage.subtitle
+        ),
+        React.createElement(
+            "p",
+            null,
+            webpage.options.length > 0 ? "Here are the options" : "There are no options"
+        ),
+        React.createElement(
+            "p",
+            null,
+            webpage.options.length
+        ),
+        React.createElement(
+            "ol",
+            null,
+            React.createElement(
+                "li",
+                null,
+                "Item one"
+            ),
+            React.createElement(
+                "li",
+                null,
+                "Item two"
+            )
+        ),
+        React.createElement(
+            "form",
+            { onSubmit: onFormSubmit },
+            React.createElement("input", { type: "text", name: "option" }),
+            React.createElement(
+                "button",
+                null,
+                "Add option"
+            )
+        )
+    );
+    ReactDOM.render(template, appRoot);
+};
 
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+render();
--- src/app.js
+++ src/app.js
@@ -3,40 +3,43 @@ console.log("app.js is running!!");
 const webpage = {
     title: "Random Choice",
     subtitle: "JSX from app.js file",
-    options: ["One", "Two"]
+    options: []
 }
 
+const onFormSubmit = (e) => {
+    e.preventDefault(); //prevent fullpage refresh
+    const option = e.target.elements.option.value; //get value from form
 
-//render subtitle only if it actually exists
-const template = (
-    <div>
-        <h1>{webpage.title}</h1>
-        {webpage.subtitle && <p> {webpage.subtitle}</p>}
-    <p>{webpage.options.length > 0? "Here are the options:" : "There are no options"}</p>
-    </div>
-);
-
-const user = {
-    name: "Elisa",
-    location: "SP",
-    age: 26
+    if (option) {
+        webpage.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
 };
 
-function getLocation(location) {
-    if (location) {
-        return <p>Location: {location}</p>;
-    }
-}
 
-const templateTwo = (
-    <div>
-        <h1>{user.name? user.name: "Anonimous"}</h1>
-        {(user.age && user.age>=18) && <p>Age: {user.age}</p>}        
-        {getLocation(user.location)}
-        
-    </div>    
-);
+const render = () => {
+    //render subtitle only if it actually exists
+    const template = (
+        <div>
+            <h1>{webpage.title}</h1>
+            {webpage.subtitle && <p> {webpage.subtitle}</p>}
+            <p>{webpage.options.length > 0? "Here are the options" : "There are no options"}</p>
+            <p>{webpage.options.length}</p>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}> 
+                <input type="text" name="option"></input>
+                <button>Add option</button>
+            </form>
+
+        </div>
+    );
+    ReactDOM.render(template, appRoot);
+}
 
 const appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+render();
