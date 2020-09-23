00adfce5e4a54dde22559c78a47efd7adcf1a2d5
N.U <n.n.ushakova@gmail.com>
2020-09-23 00:20:07 -0400

    Replace code with components

--- src/app.js
+++ src/app.js
@@ -1,60 +1,52 @@
-console.log("app.js is running!!");
-
-const webpage = {
-    title: "Random Choice",
-    subtitle: "JSX from app.js file",
-    options: []
-}
-
-const onFormSubmit = (e) => {
-    e.preventDefault(); //prevent fullpage refresh
-    const option = e.target.elements.option.value; //get value from form
-
-    if (option) {
-        webpage.options.push(option);
-        e.target.elements.option.value = '';
-        render();
-    }
-};
-
-const onMakeDecision =() => {
-    const randomNum = Math.floor(Math.random() * webpage.options.length);
-    const option = webpage.options[randomNum];
-    alert(option);
-}
-
-const onRemoveOptions = () => {
-    webpage.options = [];
-    render();
-};
-
-
-const render = () => {
-    //render subtitle only if it actually exists
-    const template = (
+class Header extends React.Component {
+    render() {
+        return (
         <div>
-            <h1>{webpage.title}</h1>
-            {webpage.subtitle && <p> {webpage.subtitle}</p>}
-            <p>{webpage.options.length > 0? "Here are the options" : "There are no options"}</p>
-            <button disabled={webpage.options.length === 0} onClick={onMakeDecision}>What to choose?</button>
-            <button onClick={onRemoveOptions}>Remove all</button>
-            <ol>
-                {/*map over an array of options to transfer it into <ol>*/}
-                {
-                    webpage.options.map((option) => <li key={option}>{option}</li>)
-                }
-            </ol>
-            <form onSubmit={onFormSubmit}> 
-                <input type="text" name="option"></input>
-                <button>Add option</button>
-            </form>
-            
-
+            <h1>Random Choice app</h1>
+            <h2>Put your life in the hands of a computer</h2>
         </div>
-    );
-    ReactDOM.render(template, appRoot);
+        );
+    }
 }
 
-const appRoot = document.getElementById("app");
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What to choose?</button>
+            </div>
+        );
+        }
+}
 
-render();
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                text
+            </div>
+        );
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                text
+            </div>
+        );
+    }
+}
+
+const jsx = (
+    <div>
+        <h1>Title</h1>
+        <Header/>
+        <Action/>
+        <Options/>
+        <AddOption/>
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
