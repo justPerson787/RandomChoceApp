20124f9116110d60a70e73bc3c7e18daca537f88
N.U <n.n.ushakova@gmail.com>
2020-10-26 11:24:18 -0400

    Refactor stateless functional components

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-const Action = (props) => {
-    return (
-        <div>
-            <button 
-                onClick={props.handlePick}
-                disabled={!props.hasOptions}
-            >
-                What to choose?
-            </button>
-        </div>
-    );
-};
+const Action = (props) => (
+    <div>
+        <button 
+            onClick={props.handlePick}
+            disabled={!props.hasOptions}
+        >
+            What to choose?
+        </button>
+    </div>
+);
 
 export default Action;
