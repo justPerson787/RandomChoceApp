714b231e485fc8e5d93b39d1b218d510fdcf9f50
N.U <n.n.ushakova@gmail.com>
2020-10-26 09:39:21 -0400

    Convert components into class-properties syntax

--- src/components/RandomChoiceApp.js
+++ src/components/RandomChoiceApp.js
@@ -6,16 +6,29 @@ import Options from './Options';
 import OptionModal from './OptionModal';
 
 export default class RandomChoiceApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.state = {
-            options: props.options
-        };
-    }
+    state = {
+        options: []
+    };
+    handleDeleteOptions = () => { 
+        this.setState(() => ({ options: [] }));
+    };
+    handlePick = () => {
+        const randomNum = Math.floor(Math.random() * this.state.options.length);
+        alert(this.state.options[randomNum]);
+    };
+    handleDeleteOption = (optionToRemove) => {
+        this.setState((prevState) => ({
+            options: prevState.options.filter((option) => optionToRemove !== option)
+        }));
+    };
+    handleAddOption = (option) => {
+        if (!option) {
+            return "Enter valid option"
+        } else if (this.state.options.indexOf(option) > -1) {
+            return "This option already exists";
+        }   
+        this.setState((prevState) => ({ options: prevState.options.concat(option) }));
+    };
 
     componentDidMount() {
         try {
@@ -34,29 +47,8 @@ export default class RandomChoiceApp extends React.Component {
             const json = JSON.stringify(this.state.options); //return string
             localStorage.setItem('options', json);
         }
-    }
-
-    handleDeleteOptions() {
-        this.setState(() => ({ options: [] }));
-    }
-    handleDeleteOption(optionToRemove) {
-        this.setState((prevState) => ({
-            options: prevState.options.filter((option) => optionToRemove !== option)
-        }));
-    }
-    handlePick() {
-        const randomNum = Math.floor(Math.random() * this.state.options.length);
-        alert(this.state.options[randomNum]);
-    }
-    handleAddOption(option) {
-        if (!option) {
-            return "Enter valid option"
-        } else if (this.state.options.indexOf(option) > -1) {
-            return "This option already exists";
-        }     
-
-        this.setState((prevState) => ({ options: prevState.options.concat(option) }));
-    }
+    }   
+   
     render() {
         const title = 'Random Choice App';
         const subtitle = 'Put your life in the hands of a computer';        
