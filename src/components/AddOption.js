0c595c0903ba2c9c0700e527d9c777b8178eab0d
N.U <n.n.ushakova@gmail.com>
2020-10-26 09:42:16 -0400

    Convert AddOption into class-properties syntax

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -3,12 +3,8 @@ import React from 'react';
 export default class AddOption extends React.Component {
     state = {
         error: undefined
-    };
-    constructor(props) {
-        super(props);
-        this.handleAddOption = this.handleAddOption.bind(this);       
-    }
-    handleAddOption(e) {
+    };    
+    handleAddOption = (e) => {
         e.preventDefault();//prevent default form submission after call
         const option = e.target.elements.option.value.trim(); //trim removes extra blank spaces 
         const error = this.props.handleAddOption(option);
@@ -19,7 +15,7 @@ export default class AddOption extends React.Component {
         if (!error) {
             e.target.elements.option.value = '';
         }
-    }
+    };
     render() {
         return (
             <div>
