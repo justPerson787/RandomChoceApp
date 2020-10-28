bceaf3d0f16ef8f27d53227f1dbe5937f9427b6d
N.U <n.n.ushakova@gmail.com>
2020-10-28 17:41:51 -0400

    Set up SCSS with webpack

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RandomChoiceApp from './components/RandomChoiceApp';
+import './styles/style.scss';
 
 ReactDOM.render(<RandomChoiceApp options={[]}/>, document.getElementById('app'));
