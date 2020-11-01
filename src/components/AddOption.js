5a065a118346c5ecf9d587426bda42516d7b9158
N.U <n.n.ushakova@gmail.com>
2020-10-31 23:17:33 -0400

    Style option and add option area

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -20,8 +20,8 @@ export default class AddOption extends React.Component {
         return (
             <div>
                 {this.state.error && <p className="add-option-error">{this.state.error}</p>}
-               <form onSubmit={this.handleAddOption}>
-                   <input type="text" name="option"></input>
+               <form className="add-option" onSubmit={this.handleAddOption}>
+                   <input className="add-option__input" type="text" name="option"></input>
                    <button className="button">Add option</button>
                </form>
             </div>
